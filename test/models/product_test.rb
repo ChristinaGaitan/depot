4f6deb9fc70e4b9a58932496782474ebfce083d1
Christina Gaitan <christina.gaitan@gmail.com>
2025-02-24 17:21:56 -0600

    add more tests to the model

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,7 @@
 require "test_helper"
 
 class ProductTest < ActiveSupport::TestCase
-  # fixtures :products
+  fixtures :products
 
   test "product attributes must not be empty" do
     product = Product.new
@@ -27,5 +27,21 @@ class ProductTest < ActiveSupport::TestCase
 
     product.price = 1
     assert product.valid?
+  end
+
+  test "product is not valid without a unique title" do
+    product = Product.new(title: products(:pragprog).title, description: "yyy", price: 1)
+    product.image.attach(io: File.open("test/fixtures/files/lorem.jpg"), filename: "lorem.jpg", content_type: "image/jpeg")
+    
+    assert product.invalid?
+    assert_equal [ "has already been taken" ], product.errors[:title]
+  end
+
+  test "product is not valid without a unique title - i18n" do
+    product = Product.new(title: products(:pragprog).title, description: "yyy", price: 1)
+    product.image.attach(io: File.open("test/fixtures/files/lorem.jpg"), filename: "lorem.jpg", content_type: "image/jpeg")
+    
+    assert product.invalid?
+    assert_equal [ I18n.translate("errors.messages.taken") ], product.errors[:title]
   end
 end
