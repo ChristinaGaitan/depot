7d2740a0730e17227c3807adaf1a155f3b3f9c88
Christina Gaitan <christina.gaitan@gmail.com>
2025-02-24 19:00:14 -0600

    add relationships to models

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ApplicationRecord
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
+
   has_one_attached :image
 
   after_commit -> { broadcast_refresh_later_to "products"}
@@ -22,4 +25,13 @@ class Product < ApplicationRecord
       errors.add(:image, "must be a GIF, JPG or PNG image")
     end
   end
+
+  private
+    # ensure that there are no line items referencing this product
+    def ensure_not_referenced_by_any_line_item
+      unless line_items.empty?
+        errors.add(:base, "Line Items present")
+        throw :abort
+      end
+    end
 end
