8b137378dc66c4799205bf4e92f55e4f2c788ddb
Christina Gaitan <christina.gaitan@gmail.com>
2025-02-28 15:12:39 -0600

    modify email templates and tests

--- test/mailers/order_mailer_test.rb
+++ test/mailers/order_mailer_test.rb
@@ -2,18 +2,20 @@ require "test_helper"
 
 class OrderMailerTest < ActionMailer::TestCase
   test "received" do
-    mail = OrderMailer.received
-    assert_equal "Received", mail.subject
-    assert_equal [ "to@example.org" ], mail.to
-    assert_equal [ "from@example.com" ], mail.from
-    assert_match "Hi", mail.body.encoded
+    mail = OrderMailer.received(orders(:one))
+    assert_equal "Pragmatic Store Order Confirmation", mail.subject
+    assert_equal [ "dave@example.org" ], mail.to
+    assert_equal [ "depot@example.com" ], mail.from
+    assert_match /1 x The Pragmatic Programmer/, mail.body.encoded
   end
 
   test "shipped" do
-    mail = OrderMailer.shipped
-    assert_equal "Shipped", mail.subject
-    assert_equal [ "to@example.org" ], mail.to
-    assert_equal [ "from@example.com" ], mail.from
-    assert_match "Hi", mail.body.encoded
+    mail = OrderMailer.shipped(orders(:one))
+    assert_equal "Pragmatic Store Order Shipped", mail.subject
+    assert_equal [ "dave@example.org" ], mail.to
+    assert_equal [ "depot@example.com" ], mail.from
+
+    html_body = mail.html_part&.body&.decoded
+    assert_includes html_body, "<td class=\"text-right\">\n    1</td>\n  <td>&times;</td>\n\n  <td class=\"pr-2\">\n    The Pragmatic Programmer\n  </td>"
   end
 end
